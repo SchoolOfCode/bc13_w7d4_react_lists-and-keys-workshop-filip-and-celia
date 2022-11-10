f6d0d8942e7e27f60035bfab9299f8829ba57beb
Filip Ruminski <fruminski@gmail.com>
2022-11-10 18:41:08 +0000

    all done

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,14 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+
+  return <main id="main">
+  
+  {samplePosts.map((samplePost)=> {
+    return <Post key={samplePost.postId} title={samplePost.title} author={samplePost.author} date={samplePost.date} text={samplePost.text} highlights={samplePost.highlights} img={samplePost.image.link} alt={samplePost.image.alt}/>
+  })}
+  
+  </main>;
 }
 
 export default Main;
